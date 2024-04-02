8b0e3988a5e5d09213ac5077129debbbe1aa62b6
Paul Griffin Petty <me@paulpetty.com>
2024-04-02 11:07:44 -0400

    Update main.js

--- open in vs code.novaextension/Scripts/main.js
+++ open in vs code.novaextension/Scripts/main.js
@@ -1,9 +1,9 @@
 const openFile = require('./openFile.js')
 
-// exports.activate = function () {
-//   // Do work when the extension is activated
-// };
-//
+exports.activate = function () {
+  console.log('open in vs code activated')
+};
+
 // exports.deactivate = function () {
 //   // Clean up state before the extension is deactivated
 // };
